9e3dac0f2314095a79a13707a7256c01a9e515b9
José M. Ramírez <32576858+JMasr@users.noreply.github.com>
2022-09-28 15:03:33 +0200

    Update extract_data.py
    Adding a 'sorting' to concatenate the files in order.
    Adding a printable notification for each file.
    
    Windows & Linux tested,

--- extract_data.py
+++ extract_data.py
@@ -8,32 +8,34 @@ This script creates a folder "Extracted_data" inside which it extracts all the w
 '''
 
 coswara_data_dir = os.path.abspath('.')  # Local Path of iiscleap/Coswara-Data Repo
-extracted_data_dir = os.path.join(coswara_data_dir, 'extracted_data')
+extracted_data_dir = os.path.join(coswara_data_dir, 'Extracted_data')
 
 if not os.path.exists(coswara_data_dir):
-    raise ("Check the Coswara dataset directory!")
+    raise "Check the Coswara dataset directory!"
 
 if not os.path.exists(extracted_data_dir):
     os.makedirs(extracted_data_dir)  # Creates the Extracted_data folder if it doesn't exist
 
-dirs_extracted = set(map(os.path.basename, glob.glob('{}/202*'.format(extracted_data_dir))))
-dirs_all = set(map(os.path.basename, glob.glob('{}/202*'.format(coswara_data_dir))))
+dirs_extracted = set(map(os.path.basename, glob.glob(f'{extracted_data_dir}/202*')))
+dirs_all = set(map(os.path.basename, glob.glob(f'{coswara_data_dir}/202*')))
 
 dirs_to_extract = list(set(dirs_all) - dirs_extracted)
 all_file_temp = os.path.join(extracted_data_dir, "temp.tar.gz")
 
 
-def extract(infile: str):
+def extract(infile: list):
+    print(f'Extracting {infile[0].split(".a")[0]}')
     # concatenate all the *tar.gz* files
     with open(all_file_temp, 'wb') as wfp:
+        infile.sort()
         for fn in infile:
             with open(fn, 'rb') as rfp:
                 shutil.copyfileobj(rfp, wfp)
 
-        # extract the all-in-one file
-        tar = tarfile.open(all_file_temp, "r:gz")
-        tar.extractall(path=extracted_data_dir)
-        tar.close()
+    # extract the all-in-one file
+    tar = tarfile.open(all_file_temp, "r:gz")
+    tar.extractall(path=extracted_data_dir)
+    tar.close()
 
 
 for d in dirs_to_extract:
